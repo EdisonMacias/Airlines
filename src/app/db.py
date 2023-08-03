f3a0ba8e3f857b1f45cd0ef3ab68fc3e964a2489
EdisonMacias <edisonmacias2012@gmail.com>
2023-08-03 10:06:57 -0500

    n

--- src/app/db.py
+++ src/app/db.py
@@ -8,19 +8,13 @@ load_dotenv()  # Toma las variables de entorno de .env
 
 # Configuración de la conexión MySQL
 
+app.config['MYSQL_USER'] = 'b3fa59e401344c'
+app.config['MYSQL_PASSWORD'] = '93cffd16'
+app.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
+app.config['MYSQL_DB'] = 'heroku_3544c227dec389d'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-
-database_url = os.environ.get('DATABASE_URL')
-if database_url:
-    url_parts = urlparse(database_url)
-    print( url_parts.username)
-    app.config['MYSQL_USER'] = url_parts.username
-    app.config['MYSQL_PASSWORD'] = url_parts.password
-    app.config['MYSQL_HOST'] = url_parts.hostname
-    app.config['MYSQL_DB'] = url_parts.path.lstrip('/')
-    app.config['MYSQL_PORT'] = url_parts.port  # Agrega esta línea para especificar el puerto
-
-# Crea la instancia de MySQL
+# Crear la instancia de MySQL
 mysql = MySQL(app)
 
 
