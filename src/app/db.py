bc72204f60650d78a2263bc8cad3f423dac8fc76
EdisonMacias <edisonmacias2012@gmail.com>
2023-08-03 00:17:13 -0500

    m

--- src/app/db.py
+++ src/app/db.py
@@ -12,10 +12,21 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # Obtén la URL de conexión de la base de datos de la variable de entorno DATABASE_URL
 database_url = os.environ.get('DATABASE_URL')
 if database_url:
-    mysql_info = database_url.split('//')[0].split(':')
-    app.config['MYSQL_USER'] = mysql_info[0]
-    app.config['MYSQL_PASSWORD'] = mysql_info[0].split('@')[0]
-    app.config['MYSQL_HOST'] = mysql_info[0].split('@')[0].split('/')[0]
-    app.config['MYSQL_DB'] = mysql_info[0].split('@')[0].split('/')[0].split('?')[0]
+    # Remove 'mysql://' from the beginning of the URL
+    db_info = database_url.replace('mysql://', '')
+
+    # Extract user and password
+    user_pass, host_db = db_info.split('@')
+    user, password = user_pass.split(':')
+
+    # Extract host and database name
+    host, database = host_db.split('/')
+
+    # Update app configuration
+    app.config['MYSQL_USER'] = user
+    app.config['MYSQL_PASSWORD'] = password
+    app.config['MYSQL_HOST'] = host
+    app.config['MYSQL_DB'] = database
+
 
 
