3f93d7a21d5dd4ce752a8e9e153ddd2b11153cd5
EdisonMacias <edisonmacias2012@gmail.com>
2023-08-03 00:27:41 -0500

    n

--- src/app/db.py
+++ src/app/db.py
@@ -2,31 +2,21 @@ from app import app
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
+from parse import urlparse
 
 load_dotenv()  # Toma las variables de entorno de .env
 
 # Configuración de la conexión MySQL
 
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-# Obtén la URL de conexión de la base de datos de la variable de entorno DATABASE_URL
+
 database_url = os.environ.get('DATABASE_URL')
 if database_url:
-    # Remove 'mysql://' from the beginning of the URL
-    db_info = database_url.replace('mysql://', '')
-
-    # Extract user and password
-    user_pass, host_db = db_info.split('@')
-    user, password = user_pass.split(':')
-
-    # Extract host and database name
-    host, database = host_db.split('/')
-
-    # Update app configuration
-    app.config['MYSQL_USER'] = user
-    app.config['MYSQL_PASSWORD'] = password
-    app.config['MYSQL_HOST'] = host
-    app.config['MYSQL_DB'] = database
+    url_parts = urlparse(database_url)
+    app.config['MYSQL_USER'] = url_parts.username
+    app.config['MYSQL_PASSWORD'] = url_parts.password
+    app.config['MYSQL_HOST'] = url_parts.hostname
+    app.config['MYSQL_DB'] = url_parts.path.lstrip('/')
 
 mysql = MySQL(app)
 
